01b524cf1c35f42e2810ff71cf315ad6ed62df71
TeenaSahu406 <teenasahu0406@gmail.com>
2026-05-06 13:53:51 +0530

    UC2

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
-
 public class QuantityMeasurementApp {
 
+    // Feet class
     static class Feet {
         private final double value;
 
@@ -24,23 +23,56 @@ public class QuantityMeasurementApp {
         }
     }
 
+    // Inches class (same structure)
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    // Static method for Feet equality
+    public static boolean compareFeet(double val1, double val2) {
+        Feet f1 = new Feet(val1);
+        Feet f2 = new Feet(val2);
+        return f1.equals(f2);
+    }
+
+    // Static method for Inches equality
+    public static boolean compareInches(double val1, double val2) {
+        Inches i1 = new Inches(val1);
+        Inches i2 = new Inches(val2);
+        return i1.equals(i2);
+    }
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        // Hardcoded values (as per UC2 requirement)
+        double feet1 = 1.0;
+        double feet2 = 1.0;
 
-        System.out.print("Enter first value in feet: ");
-        double input1 = scanner.nextDouble();
+        double inch1 = 1.0;
+        double inch2 = 1.0;
 
-        System.out.print("Enter second value in feet: ");
-        double input2 = scanner.nextDouble();
+        boolean feetResult = compareFeet(feet1, feet2);
+        boolean inchResult = compareInches(inch1, inch2);
 
-        Feet value1 = new Feet(input1);
-        Feet value2 = new Feet(input2);
-
-        boolean result = value1.equals(value2);
-
-        System.out.println("Are values equal? " + result);
-
-        scanner.close();
+        System.out.println("Feet equality: " + feetResult);
+        System.out.println("Inch equality: " + inchResult);
     }
 }
